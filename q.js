898f3f128340a118de280cbbe806736fde09fa43
ayou9889 <65youbifh@gmail.com>
2026-01-01 12:24:52 +0100

    Update q.js

--- q.js
+++ q.js
@@ -163,13 +163,13 @@ search.addEventListener("input",(ev)=>{
         container.innerHTML=`
             <div class="i">ابحث عن طريق كتابة اسم السورة أو مكان نزولها أو عدد آياتها.</div>
         `;
-    search.focus();
 });
 searchd.appendChild(search);
 searchd.appendChild(logo);
 searchC.appendChild(searchd);
 document.body.appendChild(container);
 document.body.appendChild(searchC);
+
 
 
 
